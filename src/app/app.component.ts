c80644b1eadb0c759f19c0b5fe64ebc0f16ad185
sebastianRavenna <sebastian.ravenna@gmail.com>
2024-12-12 22:32:06 -0300

    Primera subida

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, MainComponent],
   templateUrl: './app.component.html',
+  
+  // template: `
+  //   <h1>Bienvenido Yo</h1>  
+  // `,
+  // tilde al reves ` alt+96 
+  
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'clase2';
+  Name = '';
+  
 }
---
+++ src/app/main/main.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  imports: [],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.css'
+})
+export class MainComponent {
+  title = 'Trabajo Práctico N° 1';
+  content = "Doble Binding"
+}
